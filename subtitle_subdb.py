c472ef376c6116d378ef3d65618fcda3c8b7dac5
Srinath R <srinath132@gmail.com>
2014-03-24 22:18:18 +0530

    Hacked the first version of getting subtitle from thesubdb.

---
+++ subtitle_subdb.py
@@ -0,0 +1,45 @@
+import urllib2
+import urllib
+import os
+
+def get_hash(name):
+    readsize = 64*1024
+    try:
+        source_dir = '/home/pydomic/Desktop/TV/'
+        fullname = os.path.join(source_dir,name)
+        with open(fullname,'rb') as f:
+            size = os.path.getsize(fullname)
+            data = f.read(readsize)
+            f.seek(-readsize,os.SEEK_END)
+            data += f.read(readsize)
+        return hashlib.md5(data).hexdigest()
+
+    except IOError as err:
+        print str(err)
+
+
+def get_subtitle(filename):
+    video_hash = get_hash(filename)
+    url = 'http://api.thesubdb.com/'
+    try:
+        query = urllib.urlencode({'action':'download','hash':video_hash,'language':'en'})
+        request = urllib2.Request(url + '?' + query)
+        request.add_header('User-Agent','SubDB/1.0 (tvshelf/0.1; http://github.com/thegyro/tvshelf)')
+        response = urllib2.urlopen(request)
+        if response.getcode() == 200:
+            with open(filename + '.srt','w') as sub_f:
+                f.write(response.read())
+    
+        elif reponse.getcode() == 404:
+            raise Exception('Subtitle Not Found')
+
+        else:
+            raise Exception('Bad Request')
+            
+    except (urllib2.URLError,Exception,IOError) as err:
+        print str(err)
+    
+    
+            
+            
+            
