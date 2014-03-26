ea753254d20e2f966b7bfa73989a8146e0c0b3a5
Srinath R <srinath132@gmail.com>
2014-03-26 17:20:52 +0530

    Fixed some errors in subtitle_subdb.py.

--- subtitle_opensub.py
+++ subtitle_opensub.py
@@ -2,21 +2,21 @@ import os
 from pythonopensubtitles.opensubtitles import OpenSubtitles
 from pythonopensubtitles.utils import File
 
-source_dir = '/home/pydomic/Watch/Desktop/TV'
+source_dir = '/home/pydomic/Desktop/TV'
 
 def get_subtitle(filename):
     os_sub = OpenSubtitles()
     try:
         token = os_sub.login('thegyro','idontlikepasswords')
-        if os.path.exists(os.path.join(sourcedir,filename)):
-            video_file = File(os.path.join(sourcedir,filename))
+        if os.path.exists(os.path.join(source_dir,filename)):
+            video_file = File(os.path.join(source_dir,filename))
             video_hash = video_file.get_hash()
             video_size = video_file.size
             sub_param = {'sublanguageid':'English','moviehash':video_hash,'moviebytesize': video_size}
             subtitles = os_sub.search_subtitles([sub_param])
             print subtitles
         else:
-            raise Exception('FileError')
+            raise Exception("File doesn't exist")
         
     except Exception as ex:
         print str(ex)
--- subtitle_subdb.py
+++ subtitle_subdb.py
@@ -1,8 +1,9 @@
 import urllib2
 import urllib
 import os
+import hashlib
 
- source_dir = '/home/pydomic/Desktop/TV/'
+source_dir = '/home/pydomic/Desktop/TV/'
  
 def get_hash(name):
     readsize = 64*1024
@@ -20,22 +21,18 @@ def get_hash(name):
 
 
 def get_subtitle(filename):
-    video_hash = get_hash(filename)
     url = 'http://api.thesubdb.com/'
     try:
+        video_hash = get_hash(filename)
         query = urllib.urlencode({'action':'download','hash':video_hash,'language':'en'})
         request = urllib2.Request(url + '?' + query)
         request.add_header('User-Agent','SubDB/1.0 (tvshelf/0.1; http://github.com/thegyro/tvshelf)')
+        print 'Downloading Subtitle for %s' % filename
         response = urllib2.urlopen(request)
         if response.getcode() == 200:
-            with open(os.path.join(source_dir,filename + '.srt','w')) as sub_f:
-                f.write(response.read())
-    
-        elif reponse.getcode() == 404:
-            raise Exception('Subtitle Not Found')
-
-        else:
-            raise Exception('Bad Request')
+            with open(os.path.join(source_dir,filename.split('.')[0] + '.srt' ),'w') as sub_f:
+                sub_f.write(response.read())
+                print '\nDownload Successful'
             
-    except (urllib2.URLError,Exception,IOError) as err:
+    except (urllib2.URLError,IOError) as err:
         print str(err)
